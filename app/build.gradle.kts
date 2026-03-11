66badd65def143e4e95df4d6baa3e22cc104b422
TIVIT Valmir <tivit_valmir@acd.grupotiradentes.com>
2026-03-11 10:26:40 -0300

    add initial layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
