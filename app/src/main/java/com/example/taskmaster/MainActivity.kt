dcd27074c6e7c8ca53bc66360fc10105508a652e
Matheus Guilherme <zmatheusguilherme2@gmail.com>
2026-03-11 16:05:20 -0300

    [🐛] Fix
    correção de erros de import

--- app/src/main/java/com/example/taskmaster/MainActivity.kt
+++ app/src/main/java/com/example/taskmaster/MainActivity.kt
@@ -1,10 +1,13 @@
+package com.example.taskmaster
+
+import Tarefa
+import TarefaAdapter
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.taskmaster.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
